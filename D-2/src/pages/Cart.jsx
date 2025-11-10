72573b80436f7adb1e849394f5c2946574e185c5
kanishkarramamoorthi <kanishkarramamoorthi@gmail.com>
2025-11-10 11:25:01 +0530

    second commit

--- D-2/src/pages/Cart.jsx
+++ D-2/src/pages/Cart.jsx
@@ -11,37 +11,32 @@ function Cart() {
     const savedCart = JSON.parse(localStorage.getItem("Cart")) || [];
     setCartItems(savedCart);
   }, []);
-  const removeItem = (index) => {
-    const updatedCart = cartItems.filter((_, i) => i !== index);
-    setCartItems(updatedCart);
-    localStorage.setItem("Cart", JSON.stringify(updatedCart));
+  const clearCart = () => {
+    localStorage.removeItem("Cart");  
+    setCartItems([]);
   };
   const totalPrice = cartItems.reduce((total, item) => total + item.price, 0)
   return (
     <div>
       <h2>Your Cart</h2>
-      {cartItems.length === 0 ? (
-        <p>Your cart is empty.</p>
-      ) : (
         <>
-          <div className="cart-items">
+          <div>
             {cartItems.map((item, index) => (
-              <div key={index} className="cart-item">
+              <div key={index}>
                 <img src={item.img} alt={item.name} />
-                <div className="cart-details">
+                <div>
                   <h3>{item.name}</h3>
                   <p>Price: ${item.price}</p>
-                  <button onClick={() => removeItem(index)}>Remove</button>
                 </div>
               </div>
             ))}
           </div>
-          <h3 className="total">Total: ${totalPrice}</h3>
+          <h3 >Total: ${totalPrice}</h3>
          <button onClick={change}>Gpay</button>  
          <button onClick={change}>Phonepay</button>
-         <button onClick={change}>Cash</button>
+         <button onClick={change}>Cash</button><br />
+          <button onClick={clearCart} style={{background:'red'}}>Clear</button>
         </>
-      )}
     </div>
   );
 }
